fe30aa816ccbde0585c3c041eb3aedf56971a3e1
Braulio <bherrera@inovabiz.com>
2025-06-09 16:54:28 -0400

    feat() cambios de api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://back-municipalidad-production.up.railway.app'
+  apiUrl: 'http://back-municipalidad-production.up.railway.app/api'
 }; 
